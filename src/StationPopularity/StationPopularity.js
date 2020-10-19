674e0d9cc9a1323c9c4f56fc6bcbf3da7bafa615
Ri Le <r.le@columbia.edu>
2020-10-18 22:17:53 -0400

    CSS refactor

--- src/StationPopularity/StationPopularity.js
+++ src/StationPopularity/StationPopularity.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import styles from "./StationPopularity.module.css";
 const StationPopularity = ({ rank, stations_in_nta, nta_name }) => {
   return rank ? (
     <section>
-      <h3>Popularity Ranking</h3>
-      <div>
+      <h3 className={styles.heading}>Popularity Ranking</h3>
+      <div className={styles.description}>
         {rank} of {stations_in_nta} in {nta_name}
       </div>
     </section>
