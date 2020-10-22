3bb0dcdcdc3731091a1b4ebfa7ec63bdc6ca0a13
Ri Le <r.le@columbia.edu>
2020-10-22 01:42:55 -0400

    animated modal

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,56 +1,69 @@
-import React from "react";
-import { useSpring, animated } from "react-spring";
+import React, { useState } from "react";
+import { useTransition, animated } from "react-spring";
 
 import styles from "./Modal.module.css";
 import { ReactComponent as Bike } from "./../images/undraw_bike_ride_7xit.svg";
 
 const Modal = (props) => {
-  const values = useSpring({ opacity: 1, from: { opacity: 0 } });
+  const [show, set] = useState(true);
+
+  const transitions = useTransition(show, null, {
+    from: { opacity: 0, zIndex: 100 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+
   const { toggle } = props;
   const stopPropogation = (e) => {
     e.stopPropagation();
   };
 
-  return (
-    <animated.div style={values} className={styles.modal} onClick={toggle}>
-      <div className={styles.modalContent} onClick={stopPropogation}>
-        <button
-          type="button"
-          className={styles.close}
-          data-dismiss="modal"
-          aria-label="Close"
-          onClick={toggle}
-        >
-          <span aria-hidden="true">&times;</span>
-        </button>
+  return transitions.map(
+    ({ item, key, props }) =>
+      item && (
+        <animated.div key={key} style={props}>
+          <div className={styles.modal} onClick={() => set()}>
+            <div className={styles.modalContent} onClick={stopPropogation}>
+              <button
+                type="button"
+                className={styles.close}
+                data-dismiss="modal"
+                aria-label="Close"
+                onClick={() => set()}
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
 
-        <figure>
-          <Bike
-            title="Cycling illustration (Katerina Limpitsouni)"
-            className={styles.illustration}
-            viewBox="0 0 800.25287 695.42298"
-          />
-        </figure>
+              <figure>
+                <Bike
+                  title="Cycling illustration (Katerina Limpitsouni)"
+                  className={styles.illustration}
+                  viewBox="0 0 800.25287 695.42298"
+                />
+              </figure>
 
-        <h1>CitiBike Activity Viewer</h1>
+              <h1>CitiBike Activity Viewer</h1>
 
-        <p>
-          When is the best time to take a CitiBike in your area? Use this app to
-          find out which Citi Bike stations are free during a specific time of
-          day, or explore stations around the city and find out when your
-          favorite stations are the busiest.
-        </p>
+              <p>
+                When is the best time to take a CitiBike in your area? Use this
+                app to find out which Citi Bike stations are free during a
+                specific time of day, or explore stations around the city and
+                find out when your favorite stations are the busiest.
+              </p>
 
-        <h3 className={styles.heading}>Instructions</h3>
-        <p className={styles.instructions}>
-          Click on a station on the map. A histogram will appear on the sidebar
-          that shows the ride distribution across all 24 hours of the day.
-        </p>
-        <div>
-          <button onClick={toggle}>Close</button>
-        </div>
-      </div>
-    </animated.div>
+              <h3 className={styles.heading}>Instructions</h3>
+              <p className={styles.instructions}>
+                Click on a station on the map. A histogram will appear on the
+                sidebar that shows the ride distribution across all 24 hours of
+                the day.
+              </p>
+              <div>
+                <button onClick={() => set()}>Close</button>
+              </div>
+            </div>
+          </div>
+        </animated.div>
+      )
   );
 };
 
