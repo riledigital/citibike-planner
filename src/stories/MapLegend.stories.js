b8165d9bfbe9656c7d40b24b7b5f64d9ebf1ea33
Ri Le <r.le@columbia.edu>
2020-10-24 12:22:34 -0400

    added legend component

---
+++ src/stories/MapLegend.stories.js
@@ -0,0 +1,17 @@
+import React from "react";
+import MapLegend from "../MapLegend/MapLegend";
+
+// This default export determines where your story goes in the story list
+export default {
+  title: "Map Legend",
+  component: MapLegend,
+};
+
+const Template = (args) => <MapLegend data={[0, 1, 2, 3, 4]} {...args} />;
+
+export const Loaded = Template.bind({});
+
+Loaded.args = {
+  // data: null,
+  // data: [...Array(5).keys()],
+};
