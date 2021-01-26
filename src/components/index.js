17b71cd70e37981babcb35e9777224f873e4ff8f
Ri <ri.le@upstatement.com>
2021-01-26 15:50:46 -0500

    deleted babel-loader messed up dependency

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+
+export { default as ActionButton } from './ActionButton';
+export { default as CircleLegend } from './CircleLegend ';
+export { default as Footer } from './Footer';
+export { default as Header } from './Header';
+export { default as LiveStatus } from './LiveStatus';
+
