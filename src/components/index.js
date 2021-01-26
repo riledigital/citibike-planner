f6d7289dd55efdf1ebc4db334ddbcf717b5927b9
Ri <ri.le@upstatement.com>
2021-01-26 18:18:26 -0500

    move files

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,9 @@ export { default as CircleLegend } from './CircleLegend ';
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as LiveStatus } from './LiveStatus';
+export { default as Modal } from './Modal';
+export { default as StationActivity } from './StationActivity';
+export { default as StationHeader } from './StationHeader';
+export { default as StationPopularity } from './StationPopularity';
+export { default as StationRanking } from './StationRanking';
 
