a4c4d7d6f3c73702f977e5fd2cc029ee69a02fa1
Ri <ri.le@upstatement.com>
2021-01-26 21:52:30 -0500

    refactor data fetching out of map

--- src/modules/Map/Map.js
+++ src/modules/Map/Map.js
@@ -4,46 +4,16 @@ import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 import { styleDefault, activityMarker } from "./modules/Map/MapStyles";
 import { throttle } from "lodash";
+import Data from "./../../modules/Data";
 
-// import Ranking from "./Ranking/Ranking";
-const Map = () => {
+const Map = ({ sfx, stationGeos }) => {
   const [map, setMap] = useState(null);
-  const [currentStation, setCurrentStation] = useState({});
-  const [aggData, setAggData] = useState(null);
-  const [stationStatus, setStationStatus] = useState(null);
   const [stationGeo, setStationGeo] = useState(null);
-  const [lastUpdated, setLastUpdated] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [showModal, setShowModal] = useState(true);
-  const [ranking, setRanking] = useState({});
-  const [soundOn, setSound] = useState(false);
-  const [sfx, setSfxManager] = useState(null);
 
   mapboxgl.accessToken = process.env.REACT_APP_MAPBOX;
-
-  function toggleModal(e) {
-    setShowModal(!showModal);
-  }
-
-  const handleStationClick = (station) => {
-    const queryElement = document.querySelector("#stationHeader");
-    if (queryElement) {
-      queryElement.scrollIntoView({
-        behavior: "smooth",
-      });
-    }
-    setCurrentStation(station);
-  };
-
   let mapContainer = React.createRef();
-
   const markerUrl = `${process.env.PUBLIC_URL}/custom_marker.png`;
-
   useEffect(() => {
-    const sfx = new Audio();
-    setSfxManager(sfx);
-    sfx.play("sfxBike1");
-
     const map = new mapboxgl.Map({
       container: mapContainer,
       style: "mapbox://styles/mapbox/light-v10",
@@ -56,107 +26,24 @@ const Map = () => {
     });
 
     setLoading(true);
-    // Async load everything
-    Promise.allSettled([
-      fetch(`${process.env.PUBLIC_URL}/data/aggs_by_hour.json`).then((resp) =>
-        resp.json()
-      ),
-      fetch(
-        `${process.env.PUBLIC_URL}/data/station_info.geojson`
-      ).then((resp) => resp.json()),
-      fetch(
-        "https://gbfs.citibikenyc.com/gbfs/en/station_status.json"
-      ).then((resp) => resp.json()),
-    ]).then((data) => {
-      setAggData(data[0].value);
-      setStationGeo(data[1].value);
+    const DataManager = new Data();
 
+    const fetchData = async () => {
+      const results = await DataManager.startFetching();
+      setAggData(results[0].value);
+      setStationGeo(results[1].value);
       let allStationsStatus = {};
       const fetchedData = data[2].value;
-
       fetchedData["data"]["stations"].forEach((record) => {
         allStationsStatus[record.station_id] = { ...record };
-        return record;
       });
-
-      setLoading(false);
+      setStationStatus(results[2].value);
       setStationStatus(allStationsStatus);
-
       setLastUpdated(new Date(fetchedData["last_updated"] * 1000));
-
-      map.on("load", function () {
-        map.loadImage(markerUrl, function (error, img) {
-          if (error) throw error;
-          map.addImage("custom-marker", img);
-          map.addSource("stationSource", {
-            type: "geojson",
-            data: data[1].value,
-          });
-          map.addLayer(activityMarker);
-        });
-        setMap(map);
-        map.addControl(
-          new MapboxGeocoder({
-            accessToken: mapboxgl.accessToken,
-            mapboxgl: mapboxgl,
-          })
-        );
-
-        const scale = new mapboxgl.ScaleControl({
-          maxWidth: 80,
-          unit: "imperial",
-        });
-
-        map.addControl(scale);
-
-        const playThrottled = throttle(() => sfx.play("scrolling"), 100);
-        const geolocate = new mapboxgl.GeolocateControl({
-          positionOptions: {
-            enableHighAccuracy: true,
-          },
-          trackUserLocation: true,
-        });
-        map.addControl(geolocate);
-        map.on("drag", function (e) {
-          playThrottled();
-        });
-
-        map.on("zoom", function (e) {
-          playThrottled();
-        });
-      });
-    });
-
-    // "https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png",
-    map.on("click", "stationLayer", function (e) {
-      sfx.play("click");
-      let feature = e.features[0].properties;
-      var coordinates = e.features[0].geometry.coordinates.slice();
-      var description = e.features[0].properties.name;
-
-      // Ensure that if the map is zoomed out such that multiple
-      // copies of the feature are visible, the popup appears
-      // over the copy being pointed to.
-      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-      }
-
-      new mapboxgl.Popup()
-        .setLngLat(coordinates)
-        .setHTML(description)
-        .addTo(map);
-
-      handleStationClick(feature);
-    });
+    };
 
     return () => map.remove();
   }, []);
-
-  useEffect(() => {
-    if (sfx) {
-      sfx?.mute(!soundOn);
-    }
-  }, [sfx, soundOn]);
 
   return (
     <div id="main-map">
