481ae1900b84919cbc628b7a4b31534b470ba2b4
Ri <ri.le@upstatement.com>
2021-01-29 20:05:13 -0500

    stationpopularity

--- src/components/StationPopularity/StationPopularity.js
+++ src/components/StationPopularity/StationPopularity.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./StationPopularity.module.css";
 const StationPopularity = ({ rank, stations_in_nta, nta_name }) => {
-  if (!(rank && stations_in_nta && nta_name)) {
-    return <>No ranking available.</>;
-  }
+  const [name, setName] = useState(null);
+  useEffect(() => {
+    const nta_name_format = nta_name ? `in ${nta_name}` : "";
+    setName(nta_name_format);
+  });
   return rank ? (
     <section>
       <h3 className={styles.heading}>Popularity Ranking</h3>
       <div className={styles.description}>
-        {rank} of {stations_in_nta} in {nta_name}
+        {rank} of {stations_in_nta} {name}
       </div>
     </section>
   ) : (
