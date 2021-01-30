d4413e8f294aa5797cb2c875bdb8366befbafd5f
Ri <ri.le@upstatement.com>
2021-01-30 17:43:27 -0500

    actionbutton, header, footer

---
+++ src/components/Footer/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledFooter = styled.footer`
+  .App-footer {
+    grid-column: 1 / 10;
+    /* grid-row: 3; */
+    /* padding: 2rem; */
+  }
+
+  .App-footer {
+    align-self: end;
+    color: var(--c-text);
+    /* padding: 1rem; */
+  }
+`;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,3 @@
-import React from "react";
-import styles from "./Header.module.css";
-
-const Header = ({ toggleModal, toggleSound, soundOn }) => {
-  return (
-    <header className={styles.header}>
-      <div className={styles.branding}>
-        <span>Citi Bike Planner</span>
-      </div>
-      <nav className={styles.nav}>
-        <button onClick={toggleSound}>
-          Turn Sound {soundOn ? "Off" : "On"}
-        </button>
-        <button className={styles.help} onClick={toggleModal}>
-          Help
-        </button>
-      </nav>
-    </header>
-  );
-};
+import { default as Header } from "./Header.js";
 
 export default Header;
