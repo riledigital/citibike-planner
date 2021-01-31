90918af445b66f55b15f6eaf61c39d87dd260474
Ri <ri.le@upstatement.com>
2021-01-31 17:40:15 -0500

    credits

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -11,6 +11,9 @@ import {
   StyledTitle,
   StyledAttribution,
   StyledCloseButton,
+  StyledSummary,
+  StyledDetails,
+  StyledButtonWrappers,
 } from "./styles.js";
 
 const Modal = ({ toggle, soundOn, toggleSound }) => {
@@ -43,21 +46,18 @@ const Modal = ({ toggle, soundOn, toggleSound }) => {
               </StyledCloseButton>
               <StyledTitle>Citi Bike Planner</StyledTitle>
               <StyledAttribution>By Ri Le</StyledAttribution>
-
               <figure>
                 <StyledSvgBike
                   title="Cycling illustration (Katerina Limpitsouni)"
                   viewBox="0 0 800.25287 695.42298"
                 />
               </figure>
-
               <p>When is the best time to take a Citi Bike in your area?</p>
               <p>
                 Use this app to find out which Citi Bike stations are free
                 during a specific time of day, or explore stations around the
                 city and find out when your favorite stations are the busiest.
               </p>
-
               <StyledHeading>Instructions</StyledHeading>
               <StyledInstructions>
                 Click on a station on the map. A histogram will appear on the
@@ -65,15 +65,55 @@ const Modal = ({ toggle, soundOn, toggleSound }) => {
                 the day. This is a personal project unaffiliated with Motivate
                 or Citi Bike.
               </StyledInstructions>
+              <StyledDetails>
+                <StyledSummary>How is popularity calculated?</StyledSummary>
+                <p>
+                  We rank rides by the average number of trips started at each
+                  stations, grouped by Neighborhood Tabulation Areas (NTAs),
+                  which were created to predict population counts in New York
+                  City at a level finer than Census Tracts. Neighborhoods are
+                  loosely defined, but NTA's provide a sufficient rough
+                  estimate.
+                </p>
+                <p>
+                  Data is aggregated from{" "}
+                  <a href="https://www.citibikenyc.com/system-data">
+                    CitiBike System Data
+                  </a>
+                  .
+                </p>
+              </StyledDetails>
+              <StyledDetails>
+                <StyledSummary>How did you make this?</StyledSummary>
+                <p>
+                  Data aggregation was performed using Python and the geopandas
+                  library. This front-end was built with create-react-app and
+                  deployed to Netlify. Visualizations were made with a
+                  combination of D3 and React, and the map is powered by Mapbox
+                  GL JS.{" "}
+                  <a href="https://github.com/rl2999/citibike-planner">
+                    The source is available on GitHub.
+                  </a>
+                </p>
+              </StyledDetails>
 
-              <div>
+              <p>
+                2021 App created by <a href="https://riledigital.com">Ri Le</a>.{" "}
+                <a href="https://github.com/riledigital">@riledigital</a>
+              </p>
+
+              <a href="https://member.citibikenyc.com/map/" target="_blank">
+                Go to the Official CitiBike Map
+              </a>
+
+              <StyledButtonWrappers>
                 <StyledButtonSound onClick={toggleSound}>
                   Sound {soundOn ? "Off" : "On"}
                 </StyledButtonSound>
                 <StyledButtonSound onClick={() => setShow()}>
                   Explore
                 </StyledButtonSound>
-              </div>
+              </StyledButtonWrappers>
             </StyledModalContent>
           </StyledModal>
         </animated.div>
