1602d30761d1029b0d0f9b06340bdf177901462f
Ri Le <rl2999@columbia.edu>
2022-04-28 11:18:58 -0400

    Add click handler

--- MapContainer/ReactMap/ReactMap.tsx
+++ MapContainer/ReactMap/ReactMap.tsx
@@ -3,24 +3,54 @@ import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import styles from "./ReactMap.module.css";
 import clsx from "clsx";
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { selectStationGeoJson } from "common/store/AppSlice";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectCurrentStation,
+  selectStationGeoJson,
+  setSelectedStationId,
+} from "common/store/AppSlice";
 
-const layerStyle = {
-  id: "point",
+const layerStyleStations = {
+  id: "stationsPoints",
   type: "circle",
   paint: {
     "circle-radius": 10,
     "circle-color": "#007cbf",
   },
 };
+
+const layerStyleStationsText = {
+  id: "stationsText",
+  type: "symbol",
+  layout: {
+    "text-field": ["get", "name"],
+    "text-offset": [0, -1.5],
+    "text-size": 12,
+  },
+  paint: {
+    "text-color": "#000000",
+  },
+};
 const ReactMap = () => {
-  const stationsGeoJson = useSelector(selectStationGeoJson);
+  const dispatch = useDispatch();
+  const selectedStationId = useSelector(selectCurrentStation);
+  const [viewState, setViewState] = useState({
+    latitude: 40.751678516237334,
+    longitude: -73.96923065185547,
+    zoom: 14,
+    bounds: [
+      [-74.1741943359375, 40.550330732028456],
+      [-73.66676330566406, 40.901576859936284],
+    ],
+  });
+
   return (
     <Map
+      {...viewState}
       onClick={(e) => {
         let firstFeature;
+        dispatch(setSelectedStationId(null));
         if (e.features) {
           firstFeature = e.features.at(0);
           const {
@@ -28,16 +58,18 @@ const ReactMap = () => {
           } = firstFeature;
           console.debug(`Clicked station_id: `, station_id);
           // TODO: do stuff with clicked feature
-          debugger;
+          dispatch(setSelectedStationId(station_id));
+          // debugger;
+          setViewState({
+            longitude: e.lngLat.lng,
+            latitude: e.lngLat.lat,
+            zoom: 16,
+          });
         }
       }}
-      interactiveLayerIds={["point"]}
+      interactiveLayerIds={["stationsPoints"]}
+      onMove={(evt) => setViewState(evt.viewState)}
       mapLib={maplibregl}
-      initialViewState={{
-        latitude: 40.751678516237334,
-        longitude: -73.96923065185547,
-        zoom: 14,
-      }}
       style={{
         position: "fixed",
         left: 0,
@@ -54,7 +86,8 @@ const ReactMap = () => {
         type="geojson"
         data={"data/stations-with-nta.geojson"}
       >
-        <Layer {...layerStyle} />
+        <Layer {...layerStyleStations} />
+        <Layer {...layerStyleStationsText} />
       </Source>
     </Map>
   );
