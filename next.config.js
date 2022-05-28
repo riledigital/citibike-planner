c58e3f1ae79fb259e06904471737e12f17a3e270
Ri Le <rl2999@columbia.edu>
2022-05-28 12:40:46 -0400

    Add next-pwa
    Update gitignore

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   compiler: {
     // ssr and displayName are configured by default
     styledComponents: true,
   },
-};
+  pwa: {
+    dest: "public",
+  },
+});
